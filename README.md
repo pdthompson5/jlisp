d5fe28d26f375079a19a47ffb6f356b87f4ab46a
pdthompson5 <pdthompson5@crimson.ua.edu>
2022-03-11 13:45:55 -0600

    finish up test linting

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 ## Dev run command:
 `mvn exec:java -Dexec.args="test_file.lisp"`
+
+
+## Error reporting 
+
+## Testing 
+
+## Lanaguage Extentions 
--- src/test/java/jlisp/WholeProjectTests.java
+++ src/test/java/jlisp/WholeProjectTests.java
@@ -1,8 +1,5 @@
 package jlisp;
 
-
-import java.util.Collection;
-import java.util.ArrayList;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.PrintStream;
@@ -10,9 +7,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.function.Executable;
+// import org.junit.jupiter.api.TestFactory;
+// import org.junit.jupiter.api.DynamicTest;
+// import org.junit.jupiter.api.function.Executable;
+// import java.util.Collection;
+// import java.util.ArrayList;
 
 /**
  * Test class for the entire project (black box testing)
@@ -20,31 +19,37 @@ import org.junit.jupiter.api.function.Executable;
  * Every test in this class takes an input lisp file (input.lisp), runs it then asserts
  * that the output is equivalent to a sample output file(expected_output.lisp)
  * Those two files can be found in src/test/resources/${TestName}/
+ * These files are run with debug enabled so the output of each top-level expression is printed 
  */
 public class WholeProjectTests {
-    private String runLispFile(String testType){
+
+    //Run lisp file and return output
+    private String runLispFile(String testType) { 
         final ByteArrayOutputStream output = new ByteArrayOutputStream();
         PrintStream temp = System.out;
+        //Capture standard output
         System.setOut(new PrintStream(output));
 
+        //Args -> [file_name, debug]
         String[] args = new String[] {"src/test/resources/" + testType + "/input.lisp", "debug"};
-        try{
+        try {
+            //Run project with args
             Jlisp.main(args);
-        }
-        catch(Exception e){
+        } catch (Exception e) { //IO exception, should never occur
         }
 
         System.setOut(temp);
         return output.toString();
     }
 
-    private void verifyOutput(String testType, String output){
+    //Asserts that output is equal to expected_output.lisp
+    private void verifyOutput(String testType, String output) {
+        //Load in output file 
         Path path = Path.of("src/test/resources/" + testType + "/expected_output.lisp");
         String expected = "";
-        try{
+        try {
             expected = Files.readString(path);
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.err.println(e.getMessage());
             System.exit(65);
         }
@@ -52,103 +57,104 @@ public class WholeProjectTests {
         assertEquals(expected, output);
     }
 
-    private void runAndVerify(String testType){
+    //Run file for test and verify results
+    private void runAndVerify(String testType) {
         verifyOutput(testType, runLispFile(testType));
     }
 
 
     @Test
-    void testArithmetic(){
+    void testArithmetic() {
         runAndVerify("BasicArithmetic");
     }
 
     @Test
-    void testComparisons(){
+    void testComparisons() {
         runAndVerify("BasicComparisons");
     }
 
     @Test
-    void testBegin(){
+    void testBegin() {
         runAndVerify("Begin");
     }
 
     @Test
-    void testCarAndCdr(){
+    void testCarAndCdr() {
         runAndVerify("CarAndCdr");
     }
 
     @Test
-    void testCons(){
+    void testCons() {
         runAndVerify("Cons");
     }
 
     @Test
-    void testFunctions(){
+    void testFunctions() {
         runAndVerify("Functions");
     }
 
     @Test
-    void testIfStatments(){
+    void testIfStatments() {
         runAndVerify("IfStatements");
     }
 
     @Test
-    void testListQuest(){
+    void testListQuest() {
         runAndVerify("ListQuest");
     }
 
     @Test
-    void testNullQuest(){
+    void testNullQuest() {
         runAndVerify("NullQuest");
     }
 
     @Test
-    void testNumberQuest(){
+    void testNumberQuest() {
         runAndVerify("NumberQuest");
     }
 
     @Test
-    void testPrint(){
+    void testPrint() {
         runAndVerify("Print");
     }
 
     @Test
-    void testSet(){
+    void testSet() {
         runAndVerify("Set");
     }
 
     @Test
-    void testSymbolQuest(){
+    void testSymbolQuest() {
         runAndVerify("SymbolQuest");
     }
 
     @Test
-    void testWhileLoops(){
+    void testWhileLoops() {
         runAndVerify("WhileLoops");
     }
 
     @Test
-    void testRecursion(){
+    void testRecursion() {
         runAndVerify("Recursion");
     }
 
     @Test
-    void testInsertionSort(){
+    void testInsertionSort() {
         runAndVerify("InsertionSort");
     }
 
     @Test
-    void testQuoteAndEval(){
+    void testQuoteAndEval() {
         runAndVerify("QuoteAndEval");
     }
 
     @Test
-    void testPassByValue(){
+    void testPassByValue() {
         runAndVerify("PassByValue");
     }
 
     @Test
-    void test01Knapsack(){
+    void test01Knapsack() {
         runAndVerify("0-1Knapsack");
     }
     //This test is commented out since the code will result in a system exit. 
@@ -159,8 +165,8 @@ public class WholeProjectTests {
     // }
 
 
-    //The following commented out code does the same thing as the above test cases, and it does it more elegantly 
-    //However, my junit test runner does not associate junit names with test failures so it is difficult to determine which test failed 
+    //The following commented out function does the same thing as the above test cases, and it does it more elegantly 
+    //However, my maven junit test runner does not associate junit names with test failures so it is difficult to determine which test failed 
 
     // @TestFactory
     // Collection<DynamicTest> All(){
